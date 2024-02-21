cb9de8e475138d9fc0a3e674743fcfc522c63954
VrontiLykos <ethan.burns177@gmail.com>
2024-02-21 10:53:54 -0500

    Initial Commit

---
+++ CVSProject/CVSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSProjectApp.swift
+//  CVSProject
+//
+//  Created by Ethan Burns on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CVSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
